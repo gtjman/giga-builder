eba93f545af2151f603bcdcd4040d0e0cd583e8a
MOLO-7 <am8736419@gmail.com>
2022-08-03 07:37:49 +0200

    create builder setters

--- src/messages/embed/Embed.ts
+++ src/messages/embed/Embed.ts
@@ -66,6 +66,9 @@ export default class Embed implements EmbedData {
    */
   fields?: EmbedFieldData[];
 
+  /**
+   * @param data - API Embed Object
+   */
   constructor(data?: EmbedData) {
     this.author = data?.author;
     this.title = data?.title;
@@ -78,6 +81,151 @@ export default class Embed implements EmbedData {
     this.video = data?.video;
     this.fields = data?.fields;
 
-    // if (data.timestamp)
+    if (data?.timestamp)
+      this.timestamp = new Date(data.timestamp).toISOString();
+  }
+
+  /**
+   * set embed's author
+   * @param options - author name or data object
+   * @param icon - icon url
+   * @example
+   * const embed = new Embed();
+   *
+   * const avatarURL = "https://cdn.discordapp.com/avatars/697194915352019035/93ff77b264f4df9f4cc342763bd3535d.png?size=1024"
+   * // multi params
+   * embed.setAuthor("molo", avatarURL)
+   *
+   * // options object
+   * embed.setAuthor({
+   *  name: "molo",
+   *  icon_url: avatarURL
+   * })
+   */
+  setAuthor(
+    options: EmbedAuthorData | EmbedAuthorData["name"] | null,
+    icon?: EmbedAuthorData["icon_url"]
+  ): Embed {
+    if (options && typeof options === "object") {
+      if (options.name.length > 256) throw new Error();
+      this.author = options;
+    } else {
+      if (options) {
+        if (options.length > 256) throw new Error();
+        this.author = { name: options };
+        if (icon) this.author.icon_url = icon;
+      } else this.author = undefined;
+    }
+
+    return this;
+  }
+
+  /**
+   * set embed's title
+   */
+  setTitle(title: string | null): Embed {
+    if (typeof title === "string" && title.length > 256) throw new Error();
+    this.title = title ?? undefined;
+    return this;
+  }
+
+  /**
+   * set embed's description
+   */
+  setDescription(description: string | null): Embed {
+    if (typeof description === "string" && description.length > 4096)
+      throw new Error();
+    this.description = description ?? undefined;
+    return this;
+  }
+
+  /**
+   * set the url of the embed
+   */
+  setURL(url: string): Embed {
+    this.url = url;
+    return this;
+  }
+
+  // setColor(color: string): Embed {}
+
+  /**
+   * set embed's footer
+   * @param options - footer text or data object
+   * @param icon - icon url
+   * @example
+   * const embed = new Embed();
+   *
+   * const iconURL = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/600px-Instagram-Icon.png"
+   * // multi params
+   * embed.setFooter("Instagram", iconURL)
+   *
+   * // options object
+   * embed.setAuthor({
+   *  text: "Instagram",
+   *  icon_url: iconURL
+   * })
+   */
+  setFooter(
+    options: EmbedFooterData | EmbedFooterData["text"] | null,
+    icon?: EmbedFooterData["icon_url"]
+  ): Embed {
+    if (options && typeof options === "object") {
+      if (options.text.length > 2048) throw new Error();
+      this.footer = options;
+    } else {
+      if (options) {
+        if (options.length > 2048) throw new Error();
+        this.footer = { text: options };
+        if (icon) this.footer.icon_url = icon;
+      } else this.footer = undefined;
+    }
+
+    return this;
+  }
+
+  /**
+   * set embed's timestamp
+   */
+  setTimestamp(timestamp?: number | Date | null) {
+    if (timestamp === undefined) timestamp = Date.now();
+
+    this.timestamp = timestamp ? new Date(timestamp).toISOString() : undefined;
+    return this;
+  }
+
+  /**
+   * set embed's thumbnail
+   * @param options thumbnail url or data object
+   */
+  setThumbnail(
+    options: EmbedThumbnailData | EmbedThumbnailData["url"] | null
+  ): Embed {
+    if (options && typeof options === "object") this.thumbnail = options;
+    else this.thumbnail = options ? { url: options } : undefined;
+
+    return this;
+  }
+
+  /**
+   * set embed's image
+   * @param options image url or data object
+   */
+  setImage(options: EmbedImageData | EmbedImageData["url"] | null): Embed {
+    if (options && typeof options === "object") this.image = options;
+    else this.image = options ? { url: options } : undefined;
+
+    return this;
+  }
+
+  /**
+   * set embed's video
+   * @param options video url or data object
+   */
+  setVideo(options: EmbedVideoData | EmbedVideoData["url"] | null): Embed {
+    if (options && typeof options === "object") this.video = options;
+    else this.video = options ? { url: options } : undefined;
+
+    return this;
   }
 }
