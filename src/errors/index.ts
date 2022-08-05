a2123d823aaa79ba74458efb1615372c1b706517
gtjman <omarbonami2009@gmail.com>
2022-08-05 07:55:26 +0400

    Created error codes enum

---
+++ src/errors/index.ts
@@ -0,0 +1,9 @@
+export enum ErrorCodes {
+    InvalidChatInputApplicationCommandName = "Invalid application command name, command name includes invalid characters or command name length is less than 0 or more than 100",
+    ChatInputApplicationCommandDescriptionMaxLength = "Command description max length is 100 and min length is 1",
+    ChatInputApplicationCommandOptionsMaxLength = "ChatInput application command options max length is 25",
+    EmbedAuthorNameLength = "Embed author name max length is 256",
+    EmbedTitleMaxLength = "Embed title max length is 256",
+    EmbedDescriptionMaxLength = "Embed description max length in 4096",
+    EmbedFooterTextMaxLength = "Embed footer text max length is 2048"
+}
