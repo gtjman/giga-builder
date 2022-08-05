5bca31b5760c8ec5c787d85a66179275bfda128a
gtjman <omarbonami2009@gmail.com>
2022-08-05 15:30:19 +0400

    More error codes

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,5 +1,6 @@
 export enum ErrorCodes {
     InvalidApplicationCommandName = "Invalid application command name, command name includes invalid characters or command name length is less than 0 or more than 100",
+    InvalidApplicationCommandOptionName = "Invalid application command option name, option name includes invalid characters or option name length is less than 0 or more than 100",
     ChatInputApplicationCommandDescriptionMaxLength = "Command description max length is 100 and min length is 1",
     ChatInputApplicationCommandOptionsMaxLength = "ChatInput application command options max length is 25",
     EmbedAuthorNameLength = "Embed author name max length is 256",
