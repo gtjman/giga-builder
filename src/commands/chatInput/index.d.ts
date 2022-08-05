9a44539d47ed990b1d7c0d94b6891acb5628e8d3
gtjman <92214770+gtjman@users.noreply.github.com>
2022-08-05 15:14:58 +0400

    Code format changed

--- src/commands/chatInput/index.d.ts
+++ src/commands/chatInput/index.d.ts
@@ -1,4 +1,4 @@
-export interface Locales {
+interface Locales {
     da?: string;
     de?: string;
     enGB?: string;
@@ -7,7 +7,7 @@ export interface Locales {
     hr?: string;
     it?: string;
     lt?: string;
-    hu?: string; 
+    hu?: string;
     nl?: string;
     no?: string;
     fi?: string;
@@ -17,30 +17,31 @@ export interface Locales {
     cs?: string;
     el?: string;
     bg?: string;
-    ru: string;
-    uk: string;
-    hi: string;
-    th: string;
-    zhCN: string;
-    ja: string;
-    ko: string;
+    ru?: string;
+    uk?: string;
+    hi?: string;
+    th?: string;
+    zhCN?: string;
+    ja?: string;
+    ko?: string;
 }
-export type ChannelTypes = 0 | 1 | 2 | 3 | 4 | 5 | 10 | 11 | 12 | 13 | 14 | 15
-export type ApplicationCommandOptionChoiceType = {
+type ChannelTypesData = 0 | 1 | 2 | 3 | 4 | 5 | 10 | 11 | 12 | 13 | 14 | 15
+type ApplicationCommandOptionChoiceData = {
     name: string;
     name_localizations?: Locales;
     value: string | number;
 }
-export type ApplicationCommandOptionType = {
+
+type ApplicationCommandOptionData = {
     type: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11;
     name: string;
     name_localizations?: Locales;
     description: string;
     description_localizations?: Locales;
     required?: boolean;
-    choices?: ApplicationCommandOptionChoiceType[];
-    options?: ApplicationCommandOptionType[];
-    channel_types?: ChannelTypes[];
+    choices?: ApplicationCommandOptionChoiceData[];
+    options?: ApplicationCommandOptionData[];
+    channel_types?: ChannelTypesData[];
     min_value?: number;
     max_value?: number;
     min_length?: number;
@@ -48,4 +49,38 @@ export type ApplicationCommandOptionType = {
     autocomplete?: boolean;
 }
 
-export type CommandOptionTypeType = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11
+type CommandOptionTypeData = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11
+
+type ChatInputApplicationCommandData = {
+    name: string;
+    name_localizations?: Locales;
+    description: string;
+    description_localizations?: Locales;
+    options?: ApplicationCommandOptionData[];
+    dm_permission?: boolean;
+    default_permission?: boolean;
+    default_member_permissions?: string;
+}
+
+type NumberOptionData = {
+    type: 10;
+    name: string;
+    name_localizations?: Locales;
+    description: string;
+    description_localizations?: Locales;
+    required?: boolean;
+    choices?: ApplicationCommandOptionChoiceData[];
+    min_value?: number;
+    max_value?: number;
+    autocomplete?: boolean;
+}
+
+export {
+    Locales,
+    ChannelTypesData,
+    ApplicationCommandOptionChoiceData,
+    ApplicationCommandOptionData,
+    CommandOptionTypeData,
+    ChatInputApplicationCommandData,
+    NumberOptionData
+}
