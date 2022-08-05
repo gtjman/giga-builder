a943793d844a6eacab88cea10e1e97b4f1575253
MOLO-7 <am8736419@gmail.com>
2022-08-05 19:08:06 +0200

    Create size property
    points to how many instances created

--- src/commands/chatInput/ChatInputCommandBuilder.ts
+++ src/commands/chatInput/ChatInputCommandBuilder.ts
@@ -1,100 +1,118 @@
-import { ApplicationCommandOptionData, ChatInputApplicationCommandData } from ".";
-import { ErrorCodes } from "../../errors";
+import {
+  ApplicationCommandOptionData,
+  ChatInputApplicationCommandData,
+} from ".";
+// import { ErrorCodes } from "../../errors";
 
 export class ChatInputCommandBuilder {
-    private readonly type = 1;
-    private name?: string;
-    private options?: ApplicationCommandOptionData[];
-    private description?: string;
-    private dm_permission?: boolean;
-    private default_permission?: boolean;
-    private default_member_permissions?: string;
+  /**
+   * Total created chat input commands length
+   * @example
+   * const totalCommandsSize = ChatInputCommandBuilder.size;
+   *
+   * console.log(totalCommandsSize);
+   */
+  static size = 0;
+  private readonly type = 1;
+  private name?: string;
+  private options?: ApplicationCommandOptionData[];
+  private description?: string;
+  private dm_permission?: boolean;
+  private default_permission?: boolean;
+  private default_member_permissions?: string;
 
-    /**
-     * 
-     * @param { ChatInputApplicationCommandData } data - Create a ChatInputApplicationCommand from a json data (Not required)
-     * @example
-     * const command = new ChatInputCommandBuilder({
-     * name: "ping",
-     * description: "Replies with pong 🏓"
-     * })
-     */
+  /**
+   *
+   * @param { ChatInputApplicationCommandData } data - Create a ChatInputApplicationCommand from a json data (Not required)
+   * @example
+   * const command = new ChatInputCommandBuilder({
+   * name: "ping",
+   * description: "Replies with pong 🏓"
+   * })
+   */
 
-    constructor(data?: ChatInputApplicationCommandData) {
-        this.name = data?.name;
-        this.description = data?.description;
-        this.options = data?.options;
-        this.dm_permission = data?.dm_permission;
-        this.default_permission = data?.default_permission;
-        this.default_member_permissions = data?.default_member_permissions;
-    };
+  constructor(data?: ChatInputApplicationCommandData) {
+    ChatInputCommandBuilder.size++;
 
-    /**
-     * Set the name of the command
-     * @param {string} name - Name of the command, 1-32 characters
-     */
+    this.name = data?.name;
+    this.description = data?.description;
+    this.options = data?.options;
+    this.dm_permission = data?.dm_permission;
+    this.default_permission = data?.default_permission;
+    this.default_member_permissions = data?.default_member_permissions;
+  }
 
-    public setName(name: string) {
-        let ChatInputApplicationCommandNameRegex = new RegExp(/^[-_\p{L}\p{N}\p{sc=Deva}\p{sc=Thai}]{1,32}$/gi);
-        let test = ChatInputApplicationCommandNameRegex.test(name);
-        if (test) {
-            this.name = name;
-            return this;
-        } else throw new Error(ErrorCodes.InvalidApplicationCommandName);
-    };
+  /**
+   * Set the name of the command
+   * @param {string} name - Name of the command, 1-32 characters
+   */
 
-    /**
-     * Set the description of the command
-     * @param {string} description - Description of the command, 1-100 characters.
-     */
+  public setName(name: string) {
+    let ChatInputApplicationCommandNameRegex = new RegExp(
+      /^[-_\p{L}\p{N}\p{sc=Deva}\p{sc=Thai}]{1,32}$/gi
+    );
+    let test = ChatInputApplicationCommandNameRegex.test(name);
+    if (test) {
+      this.name = name;
+      return this;
+    } else throw new Error(ErrorCodes.InvalidApplicationCommandName);
+  }
 
-    public setDescription(description: string) {
-        if (description.length > 100 || description.length < 1) {
-            throw new Error(ErrorCodes.ChatInputApplicationCommandDescriptionMaxLength);
-        } else {
-            this.description = description;
-            return this;
-        };
-    };
+  /**
+   * Set the description of the command
+   * @param {string} description - Description of the command, 1-100 characters.
+   */
 
-    /**
-     * Set the dm_permission of the command
-     * @param {boolean} mode - Indicates whether the command is available in DMs with the app, only for globally-scoped commands. By default, commands are visible.
-     */
+  public setDescription(description: string) {
+    if (description.length > 100 || description.length < 1) {
+      throw new Error(
+        ErrorCodes.ChatInputApplicationCommandDescriptionMaxLength
+      );
+    } else {
+      this.description = description;
+      return this;
+    }
+  }
 
-    public setDmPermission(mode: boolean) {
-        this.dm_permission = mode;
-        return this;
-    };
+  /**
+   * Set the dm_permission of the command
+   * @param {boolean} mode - Indicates whether the command is available in DMs with the app, only for globally-scoped commands. By default, commands are visible.
+   */
 
-    /**
-     * Set the default_permission of the command
-     * @param {boolean} mode - Not recommended for use as field will soon be deprecated. Indicates whether the command is enabled by default when the app is added to a guild, defaults to true
-     */
+  public setDmPermission(mode: boolean) {
+    this.dm_permission = mode;
+    return this;
+  }
 
-    public setDefaultPermission(mode: boolean) {
-        this.default_permission = mode;
-        return this;
-    };
+  /**
+   * Set the default_permission of the command
+   * @param {boolean} mode - Not recommended for use as field will soon be deprecated. Indicates whether the command is enabled by default when the app is added to a guild, defaults to true
+   */
 
-    /**
-     * Set the default_member_permissions of the command
-     * @param {boolean} mode
-     */
+  public setDefaultPermission(mode: boolean) {
+    this.default_permission = mode;
+    return this;
+  }
 
-    public setDefaultMemberPermissions(permissions: string) {
-        this.default_member_permissions = permissions;
-        return this;
-    };
+  /**
+   * Set the default_member_permissions of the command
+   * @param {boolean} mode
+   */
 
-    /**
- * Set the options of the command
- * @param {ApplicationCommandOptionType} options - Parameters for the command, max of 25
- */
+  public setDefaultMemberPermissions(permissions: string) {
+    this.default_member_permissions = permissions;
+    return this;
+  }
 
-    public setOptions(options: ApplicationCommandOptionData[]) {
-        if (options.length > 25) throw new Error(ErrorCodes.ChatInputApplicationCommandOptionsMaxLength);
-        this.options = options;
-        return this;
-    };
+  /**
+   * Set the options of the command
+   * @param {ApplicationCommandOptionType} options - Parameters for the command, max of 25
+   */
+
+  public setOptions(options: ApplicationCommandOptionData[]) {
+    if (options.length > 25)
+      throw new Error(ErrorCodes.ChatInputApplicationCommandOptionsMaxLength);
+    this.options = options;
+    return this;
+  }
 }
